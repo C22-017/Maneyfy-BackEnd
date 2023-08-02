f80a3780625c9dcab6a177eaff5ac1f6486447a6
TegarNH <tegarnaufalh@gmail.com>
2023-08-02 07:38:56 +0700

    fix syntax error

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
-app.use(cors(corsOptions);
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
